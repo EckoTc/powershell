2b4ff5a1f440467b0b66dc51b12d631b550cee38
EckoTc <84422175+EckoTc@users.noreply.github.com>
2021-12-28 13:02:52 -0600

    Update test3.ps1

--- test3.ps1
+++ test3.ps1
@@ -1,8 +1,8 @@
 clear-host
 write-host 1 - Install VPN
 write-host 2 - Install Qualys
-write-host 3 - Install Dell Command Update
-write-host 4 - Install Bomgar
+write-host 3 - Install Bomgar
+write-host 4 - Install 
 write-host 5 - Install Power Settings
 write-host 6 - Fix Snip and Sketch
 #write-host 7 - Test test
@@ -12,19 +12,20 @@ write-host 6 - Fix Snip and Sketch
 
 $choice = read-host "Please select a number"
 
+Clear-Host
+
 if ( $choice -eq 1 )
 {
-       iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/EckoTc/Pulse/main/vpntest1.ps1'))
+$app = Pulse
 }
 
 if ( $choice -eq 2 )
 {
-        Write-Output "you selected 2"
-}
+$app = Qualys
 
 if ( $choice -eq 3 )
 {
-        Write-Output "you selected 3"
+$app = Bomgar
 }
 
 if ( $choice -eq 4 )
@@ -42,5 +43,6 @@ if ( $choice -eq 6 )
         Get-AppxPackage -allusers Microsoft.ScreenSketch | Foreach {Add-AppxPackage -DisableDevelopmentMode -Register "$($_.InstallLocation)\AppXManifest.xml"}
 }
 
-
+iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/EckoTc/$app/main/setup.ps1'))
 #iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/EckoTc/powershell/main/test3.ps1'))
+pause
