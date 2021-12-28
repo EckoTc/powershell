bbc887cf807624c8f5b0c8d2a4577948571aff5a
EckoTc <84422175+EckoTc@users.noreply.github.com>
2021-12-28 15:19:43 -0600

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,3 +1,6 @@
+function Install { iex ((New-Object System.Net.WebClient).DownloadString("https://raw.githubusercontent.com/EckoTc/$app/main/setup.ps1")) }
+
+
 clear-host
 write-host 1 - Install VPN
 write-host 2 - Install Qualys
@@ -9,23 +12,28 @@ write-host 6 - Fix Snip and Sketch
 #write-host 8 - Test test
 #write-host 9 - Test test
 #write-host 10 - Test test
-
+$app = ''
 $choice = read-host "Please select a number"
 
 Clear-Host
 
+
 if ( $choice -eq 1 )
 {
-$app = Pulse
+$app = 'Pulse'
+Install
 }
 
 if ( $choice -eq 2 )
 {
-$app = Qualys
+$app = 'Qualys'
+Install
+}
 
 if ( $choice -eq 3 )
 {
-$app = Bomgar
+$app = 'Bomgar'
+Install
 }
 
 if ( $choice -eq 4 )
@@ -40,9 +48,18 @@ if ( $choice -eq 5 )
 
 if ( $choice -eq 6 )
 {
-        Get-AppxPackage -allusers Microsoft.ScreenSketch | Foreach {Add-AppxPackage -DisableDevelopmentMode -Register "$($_.InstallLocation)\AppXManifest.xml"}
+Get-AppxPackage -allusers Microsoft.ScreenSketch | Foreach {Add-AppxPackage -DisableDevelopmentMode -Register "$($_.InstallLocation)\AppXManifest.xml"}
 }
 
-iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/EckoTc/$app/main/setup.ps1'))
-#iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/EckoTc/powershell/main/test3.ps1'))
+
+
+<#if ( $choice -le 3 )
+{
+Write-Host test $app
+iex ((New-Object System.Net.WebClient).DownloadString("https://raw.githubusercontent.com/EckoTc/$app/main/setup.ps1"))
+}
+
+#>
+
+
 pause
